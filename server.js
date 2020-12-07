9302d09d59c24ce4a5e8e7025d77a63313516ee5
Chris <slickeck@gmail.com>
2020-12-07 15:44:25 -0800

    Heroku setup

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const port = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://admin-chris:WaSwag5y@cluster0.zygrl.mongodb.net/notesDB", { 
-  useNewUrlParser: true,
-  useUnifiedTopology: true
- })
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
   app.use(express.static('client/build'));
@@ -18,6 +17,14 @@ if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging')
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
 }
+
+mongoUser = process.env.MONGO_USER;
+mongoPw = process.env.MONGO_PW;
+
+mongoose.connect("mongodb+srv://" + "mongoUser" + ":" + "mongoPw" + "@cluster0.zygrl.mongodb.net/notesDB", { 
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+ })
 
 app.use("/users/", require("./routes/usersRoute"));
 app.use("/weather_truckee/", require("./routes/weatherRouteTruckee"));
@@ -31,4 +38,3 @@ app.get("/", function(req, res){
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
-
