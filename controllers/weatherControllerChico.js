7305d9e05c2efe6bd664cb1133fa1742a8e0d0ae
Chris <slickeck@gmail.com>
2020-12-10 12:38:33 -0800

    cors anywhere

--- controllers/weatherControllerChico.js
+++ controllers/weatherControllerChico.js
@@ -12,7 +12,7 @@ exports.weatherControllerChico = (req, res) => {
   const query = "Chico";
   const apiKey = "ec56a4f85f4d58f06fce8cbb402d3d09";
   const unit = "imperial";
-  const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+unit+"&appid="+apiKey+""
+  const url = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+unit+"&appid="+apiKey+""
 
   https.get(url, function(response) {
 
