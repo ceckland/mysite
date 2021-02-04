33ae47ed3d624a93a3763c6df69387129d0bf91b
Chris Eckland <slickeck@gmail.com>
2021-02-03 17:07:40 -0800

    add Delete button

--- client/src/components/Notes.jsx
+++ client/src/components/Notes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import { useEffect, useState } from "react";
 import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
 import { Typography } from "@material-ui/core";
 
 const useStyles = makeStyles({
@@ -9,13 +10,17 @@ const useStyles = makeStyles({
     margin: '15px 0px 10px 0px;'
   },
   body: {
-    margin: '15px 0px 15px 0px;'
+    margin: '15px 0px 25px 0px;'
+  },
+  button: {
+    margin: '0px 0px 25px 0px;'
   },
 });
 
 const Notes = () => {
 
     const classes = useStyles();
+    const [authenticated, setAuthenticated] = useState(true);
 
     const [notes, setNotes] = useState([{
         title: "",
@@ -30,16 +35,27 @@ const Notes = () => {
         }).then(jsonRes => setNotes(jsonRes));
     })
 
+    function handleClick(event) {
+        event.preventDefault();
+    
+    }
+
     return (
         
         <Grid item xs={12}>
-            <Typography className={classes.title} variant="h4">Notes</Typography>
-            {notes.map(note => 
-                <div>
-                    <Typography className={classes.body} variant="subtitle1">{note.title}</Typography>
-                    <Typography className={classes.body} variant="body1">{note.content}</Typography>
-                </div>
-            )}
+            <Typography className={classes.title} variant="h4">Random Notes, Etc.</Typography>
+                
+                {notes.map(note => 
+
+                    <div>
+                        <Typography className={classes.title} variant="h5">{note.title}</Typography>
+                        <Typography className={classes.body} variant="body1">{note.content}</Typography>
+                    
+                        {authenticated ? <div><button className={classes.button} onClick={handleClick}>DELETE NOTE</button></div> : null }
+                    
+                    </div>  
+            )}   
+
          </Grid>
         
     )  
